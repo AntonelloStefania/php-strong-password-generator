e56bc3375fc585a30e58a397276c1b8cb2bc61ad
AntonelloStefania <antonelloste94@gmail.com>
2023-07-05 14:40:42 +0200

    [add] funzione randomPasswordGen

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
-if(isset($_GET['psw-length']) && $_GET['psw-length'] != ''){
 
-    $pswLength = $_GET['psw-length'];
+
+if(isset($_GET['psw-length']) && $_GET['psw-length'] != ''){
+    function randomPasswordGen() {
+        $pswLength = $_GET['psw-length'];
+        $allowedChar = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+        $pass = [];
+        $charLength = strlen($allowedChar) -1;
+        for ($i = 0; $i < $pswLength; $i++) {
+            $n = rand(0, $charLength);
+            $pass[] = $allowedChar[$n];
+        }
+        return implode($pass);
+    }
+   
 }
 
 ?>
@@ -11,18 +23,20 @@ if(isset($_GET['psw-length']) && $_GET['psw-length'] != ''){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <div class="form-group">
         <form action="index.php" method="GET">
             <input type="number" min=5 name="psw-length">
-            <button type="submit">invia</button>
+            <button type="submit" class="btn btn-success">invia</button>
+            <button name='reset' type="reset" value='Reset' class="btn btn-danger">reset</button>
         </form>
     </div>
     <div>
         <?php if(isset($_GET['psw-length']) && $_GET['psw-length'] != ''){ ?>
-            <span><?php echo $pswLength?></span>
+            <span><?php echo randomPasswordGen()?></span>
         <?php }?>
     </div>
     
